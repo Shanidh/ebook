440eb53eb3df14d40bdc2c252f2602fd34bc55ff
Shanidh <shanidprobase@gmail.com>
2022-03-08 02:43:03 -0800

    ddd

--- ebookapp/models.py
+++ ebookapp/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     username=models.CharField(max_length=10, unique=True)
     password=models.CharField(max_length=10)
 
+#ebook table
 class Ebook(models.Model):
     title = models.CharField(max_length=20)
     author = models.CharField(max_length=20)
@@ -13,6 +14,7 @@ class Ebook(models.Model):
     favourite = models.BooleanField()
     fkUser = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
+# to store token when login
 class Usertoken(models.Model):
     token = models.CharField(max_length=1000)
     fkUser = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
